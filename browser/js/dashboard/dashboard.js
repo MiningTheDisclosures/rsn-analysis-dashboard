897696450d105123ea0fbc30fc7fb40335a7e7eb
Mark Hansen <x@markthemark.com>
2016-05-07 17:11:02 -0700

    Auth wasnt stopping requests for information. Now short circuits requests

--- browser/js/dashboard/dashboard.js
+++ browser/js/dashboard/dashboard.js
@@ -1,22 +1,19 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('dashboard', {
-        url: '/dashboard',
-        templateUrl: 'js/dashboard/dashboard.html',
-        controller: 'DashboardController',
-        resolve: {
-          resolvedCompanies: function($http) {
-            return $http.get('/api/company')
-              .then(function(res) {
-                return res.data;
-              });
-          }
-        },
-        onEnter: function($state, AuthService) {
-          // Check to see if we're authed
-          if (!AuthService.isAuthenticated()) {
-            $state.go('report');
-          }
+      url: '/dashboard',
+      templateUrl: 'js/dashboard/dashboard.html',
+      controller: 'DashboardController',
+      resolve: {
+        resolvedCompanies: function($http) {
+          return $http.get('/api/company')
+            .then(function(res) {
+              return res.data;
+            });
         }
+      },
+      data: {
+        authenticate: true
+      }
     });
 });
 
